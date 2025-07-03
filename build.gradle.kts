acdbcb1e61cf62d4f6eb56dfe21dc81a5759ad46
minhnguyen-dream_tech <minhfb191@gmail.com>
2025-07-04 00:19:26 +0700

    fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,3 +3,6 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
 }
+tasks.register("publishToMavenLocal") {
+    dependsOn(":button_switch:publishReleasePublicationToMavenLocal")
+}
